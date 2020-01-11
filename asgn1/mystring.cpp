4ff25b01d7093dc7e225ca53c4ab32d9a68d6903
Victor Soriano Mendoza <vsoriano@hotp.com>
2020-01-10 18:05:12 -0800

    implement the rest of the header functions

--- asgn1/mystring.cpp
+++ asgn1/mystring.cpp
@@ -6,24 +6,34 @@ using namespace std;
 bool MyString::printAsUppercase = false;
 
 MyString::MyString() {
-    this->data = NULL;
-    this->length = 0;
-    this->printAsUppercase = false;
+    data = nullptr;
+    length = 0;
+    printAsUppercase = false;
 }
 
 MyString::MyString(char* name) {
     int length = stringLength(name);
-    this->data = new char[length];
-    for (int i = 0; i < length; ++i) {
-        this->data[i] = name[i];
+    data = new char[length+1];
+    for (int i = 0; i < length+1; ++i) {
+        data[i] = name[i];
     }
     this->length = length;
-    this->printAsUppercase = false;
+    printAsUppercase = false;
 }
 
+MyString::MyString(MyString& m) {
+    data = new char[m.length+1];
+    length = m.length;
+    printAsUppercase = m.printAsUppercase;
+    for (int i = 0; i < length+1; ++i) {
+        data[i] = m.data[i];
+    }
+}
 MyString::~MyString() {
-    delete [] data;
-    //this->data = NULL;
+    if (data != nullptr) {
+        delete [] data;
+        data = nullptr;
+    }
 }
 
 int MyString::getLength() {
@@ -33,7 +43,6 @@ int MyString::getLength() {
 void MyString::uppercase() {
     for (int i = 0; data[i] != '\0'; ++i) {
         data[i] = data[i] & ~0x20;
-        
     }
     return;
 }
@@ -43,11 +52,10 @@ void MyString::setPrintAsUppercase(bool uppercase) {
 }
 
 int MyString::stringLength(char* str) {
-    int length = 0;
-    for(int i = 0; str[i] != '\0'; ++i) {
-        ++length;
+    int i = 0;
+    for(; str[i] != '\0'; ++i) {
     }
-    return length;
+    return i;
 }
 
 MyString MyString::operator+(const MyString& m) {
@@ -59,7 +67,7 @@ MyString MyString::operator+(const MyString& m) {
         str[i] = this->data[j];
         ++i;
     }
-    for (int k = 0; k < mLength; ++k) {
+    for (int k = 0; k < mLength+1; ++k) {
         str[i] = m.data[k];
         ++i;
     }
@@ -68,21 +76,51 @@ MyString MyString::operator+(const MyString& m) {
 }
 
 MyString MyString::operator--() {
-    int newLength = length - 1;
-    char* str = new char[newLength];
-    for (int i = 0; i < newLength; ++i) {
+    length = length - 1;
+    char* str = new char[length+1];
+    for (int i = 0; i < length; ++i) {
         str[i] = data[i];
     }
-    MyString m(str);
-    return m;
+    str[length] = '\0';
+    delete [] data;
+    data = str;
+    return *this;
+}
+
+MyString MyString::operator--(int i) {
+    MyString temp = *this;
+    length = length - 1;
+    char* str = new char[length+1];
+    for (int i = 0; i < length; ++i) {
+        str[i] = data[i];
+    }
+    str[length] = '\0';
+    delete [] data;
+    data = str;
+    return temp;
+}
+
+MyString& MyString::operator=(const MyString& m) {
+    if (data) {
+        delete [] data;
+        data = nullptr;
+    }
+    length = m.length;
+    printAsUppercase = m.printAsUppercase;
+    data = new char[length+1];
+    for (int i = 0; i < length+1; ++i) {
+        data[i] = m.data[i];
+    }
+    return *this;
 }
 
 ostream& operator<<(ostream& os, const MyString& str) {
     if (MyString::printAsUppercase) {
-        char* upper = new char[str.length];
+        char* upper = new char[str.length+1];
         for (int i = 0; i < str.length; ++i) {
             upper[i] = str.data[i] & ~0x20;
         }
+        upper[str.length] = '\0';
         os << upper << "(" << str.length << ")";
         delete [] upper;
         return os;
@@ -93,18 +131,41 @@ ostream& operator<<(ostream& os, const MyString& str) {
 }
 
 istream& operator >>(istream& is, MyString& str) {
-    char name[4] = "foo";
-    is >> name;
-    str.data = name;
-    str.length = 3;
+    const unsigned int maxLength = 255;
+    char* buffer = new char[maxLength];
+    if (str.data) {
+        delete [] str.data;
+        str.data = nullptr;
+        str.length = 0;
+    }
+    char m;
+    int i = 0;
+    while(is.get(m)) {
+        if (i == (maxLength - 1)) {
+            break;
+        }
+        if (m == '\n') {
+            break;
+        }
+        buffer[i] = m;
+        ++i;
+    }
+    buffer[i] = '\0';
+    str.data = new char[i+1];
+    for (int k = 0; k < i+1; ++k) {
+        str.data[k] = buffer[k];
+    }
+    str.length = i;
+
     return is;
 }
 
 bool operator==(const MyString& str1, const MyString& str2) {
+    //TODO reimplement this using MyString::stringComp
     // is there anything to compare?
     if (!str1.data || !str1.data) {
-        // if they're both NULL, they're equal
-        if (str1.data == NULL && str2.data == NULL) {
+        // if they're both nullptr, they're equal
+        if (str1.data == nullptr && str2.data == nullptr) {
             return true;
         }
         return false;
@@ -123,43 +184,100 @@ bool operator==(const MyString& str1, const MyString& str2) {
     return true;
 }
 
-bool operator>(const MyString& str1, const MyString& str2) {
-    const char* s1 = str1.data;
-    const char* s2 = str2.data;
+int MyString::stringComp(const char* str1, const char* str2) {
+    // compares c1 to c2
+    char c1 = *str1;
+    char c2 = *str2;
+    while (c1 != '\0') {
+        //cout << "Comparing " << c1 << " " << c2 << endl;
+        if (c2 == '\0') {
+            return 1;
+        }
 
-    while (*s1 == *s2) {
-        ++s1;
-        ++s2;
+        if (c1 < c2) {
+            return -1;
+        }
+
+        if (c1 > c2) {
+            return 1;
+        }
+
+        ++str1;
+        ++str2;
+        c1 = *str1;
+        c2 = *str2;
     }
-    int cmp = *s1 - *s2;
-    cout << "Difference " << cmp << endl;
-    if (cmp > 0) {
-        return true;
+
+    if (c2 != '\0') {
+        return -1;
     }
-    return false;
+
+    return 0;
+}
+
+bool operator>(const MyString& str1, const MyString& str2) {
+    return MyString::stringComp(str1.data, str2.data) > 0;
 }
 
 bool operator<(const MyString& str1, const MyString& str2) {
-    const char* s1 = str1.data;
-    const char* s2 = str2.data;
-
-    while (*s1 == *s2) {
-        ++s1;
-        ++s2;
-    }
-    int cmp = *s1 - *s2;
-    cout << "Difference " << cmp << endl;
-    if (cmp < 0) {
-        return true;
-    }
-    return false;
+    return MyString::stringComp(str1.data, str2.data) < 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    /*
+    for (int i = 1; i < argc; ++i) {
+        cout << argv[i] << endl;
+    }
+    */
+    MyString* strings = new MyString[argc - 1];
+    for (int i = 0; i < argc-1; ++i) {
+        strings[i] = MyString(argv[i+1]);
+    }
+    for (int i = 0; i < argc-1; ++i) {
+        cout << strings[i] << " ";
+    }
+    cout << endl;
+    MyString first(strings[0]);
+    MyString last(strings[argc - 2]);
+    if (first < last) {
+        //cout << "The first string (" << argv[1] << ") ";
+        cout << "The first string (" << first << ") ";
+        //cout << "is less than the last string (" << argv[argc - 1] << ").";
+        cout << "is less than the last string (" << last << ").";
+        cout << endl;
+    } else if (first > last) {
+        cout << "The first string (" << argv[1] << ") ";
+        cout << "is greater than the last string (" << argv[argc - 1] << ").";
+        cout << endl;
+    } else if (first == last) {
+        cout << "The first string (" << argv[1] << ") ";
+        cout << "is equal to the last string (" << argv[argc - 1] << ").";
+        cout << endl;
+    }
+    for (int i = 0; i < argc-1; ++i) {
+        cout << --strings[i] << " ";
+    }
+    cout << endl;
+    MyString combined;
+    combined = strings[0]-- + --strings[argc - 2];
+    cout << combined << endl;
+    combined.uppercase();
+    cout << combined << endl;
+    MyString::setPrintAsUppercase(true);
+    for (int i = 0; i < argc-1; ++i) {
+        cout << strings[i] << " ";
+    }
+    cout << endl;
+
+    cout << strings[0] << endl;
+    cout << strings[argc - 2] << endl;
+
+    /*
     char foo[] = "foo";
     char bar[] = "bar";
     MyString m(foo);
     MyString n(bar);
+    MyString l(m);
     MyString s;
     //cout << "m length " << m.getLength() << endl;
     //cout << "n length " << n.getLength() << endl;
@@ -182,6 +300,10 @@ int main() {
 
     cout << "n: " << n << endl;
     cout << "m: " << m << endl;
+    cout << "l: " << l << endl;
+    l = n;
+    cout << "l: " << l << endl;
+    cout << "m: " << m-- << endl;
     if (n > m) {
         cout << "n is greater than m" << endl;
     } else {
@@ -193,9 +315,14 @@ int main() {
     } else {
         cout << "m is less than n" << endl;
     }
+    cout << "m: " << m << endl;
+    cin >> l;
+    cout << l << endl;
 
     //cout << "Enter a string: ";
     //cin >> s;
     //cout << s << endl;
     return 0;
+    */
+
 }
--- asgn1/mystring.h
+++ asgn1/mystring.h
@@ -11,23 +11,23 @@ class MyString {
         int length;
         static bool printAsUppercase; // initialize to false. if true, outputs
                                       // all strings as uppercase
-        int strcmp(const char*, const char*);
     public:
         MyString(); // done
         MyString(char*); //done
-        MyString(MyString&);
-        ~MyString();
+        MyString(MyString&); //done
+        ~MyString(); //done
         void uppercase(); //done
         int getLength(); //done
         static void setPrintAsUppercase(bool); //done
         MyString operator+(const MyString&); //done?
-        MyString operator--();
-        MyString operator--(int);
-        MyString& operator=(const MyString&);
+        MyString operator--(); //done
+        MyString operator--(int); //done
+        MyString& operator=(const MyString&); //done
+        static int stringComp(const char*, const char*); //done
         int stringLength(char*); //done
-        friend bool operator==(const MyString&, const MyString&);
-        friend bool operator>(const MyString&, const MyString&);
-        friend bool operator<(const MyString&, const MyString&);
+        friend bool operator==(const MyString&, const MyString&); //done
+        friend bool operator>(const MyString&, const MyString&); //done
+        friend bool operator<(const MyString&, const MyString&); //done
         friend ostream& operator <<(ostream&, const MyString&); //done
         friend istream& operator >>(istream&, MyString&);
 };
