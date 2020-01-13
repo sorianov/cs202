e1083e8f159f449c5341971ce2e533a79bb32486
Victor Soriano Mendoza <victor.sorianomendoza@gmail.com>
2020-01-13 12:37:39 -0800

    add some function documentation and remove old Makefile

--- asgn1/mystring.h
+++ asgn1/mystring.h
@@ -12,10 +12,39 @@ class MyString {
         static bool printAsUppercase; // initialize to false. if true, outputs
                                       // all strings as uppercase
     public:
+        /**
+         * Default Constructor
+         */
         MyString();
+        /**
+         * Overloaded constructor that accepts a character array.
+         *
+         * @param char* Pointer to character array which will deep copied to
+         *              data member
+         */
         MyString(char*);
+        /**
+         * Copy Constructor. Will deep copy MyString parameter to this
+         *
+         * @param MyString& A reference to the object we should copy.
+         */
         MyString(MyString&);
+        /**
+         * Destructor. Will deallocate data member if necessary.
+         */
         ~MyString();
+        /**
+         * Binary + operator overload. Will concatenate the data members of
+         * this and the passed in reference. Returns a new MyString object with
+         * its data member as the MyString reference appended to this' data
+         * member.
+         *
+         * @param MyString& A reference to the MyString object that should be
+         *                  concatenated with this
+         *
+         * @return MyString A new MyString object that has the concatenated
+         *                  string as its data member
+         */
         MyString operator+(const MyString&);
         MyString operator--();
         MyString operator--(int);
