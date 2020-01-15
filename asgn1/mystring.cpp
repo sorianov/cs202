c1d9b44366be8f8e041bc8fc6a74b51eb87e0b4f
Victor Soriano Mendoza <soriano.victorm@student.clackamas.edu>
2020-01-15 12:55:00 -0800

    change from array syntax to pointer syntax

--- asgn1/mystring.cpp
+++ asgn1/mystring.cpp
@@ -6,31 +6,38 @@ using namespace std;
 bool MyString::printAsUppercase = false;
 
 MyString::MyString() {
-    data = nullptr;
+    data = NULL;
     length = 0;
 }
 
 MyString::MyString(char* name) {
     int length = MyString::stringLength(name);
     data = new char[length];
+    char* str = data;
     for (int i = 0; i < length; ++i) {
-        data[i] = name[i];
+        *str = *name;
+        ++str;
+        ++name;
     }
-
     this->length = length;
 }
 
 MyString::MyString(MyString& m) {
     data = new char[m.length];
     length = m.length;
+    char* str1 = data;
+    char* str2 = m.data;
     for (int i = 0; i < length; ++i) {
-        data[i] = m.data[i];
+        *str1 = *str2;
+        ++str1;
+        ++str2;
     }
 }
+
 MyString::~MyString() {
     if (data) {
         delete [] data;
-        data = nullptr;
+        data = NULL;
     }
 }
 
@@ -39,8 +46,10 @@ int MyString::getLength() {
 }
 
 void MyString::uppercase() {
+    char* str = data;
     for (int i = 0; i < length; ++i) {
-        data[i] = data[i] & ~0x20;
+        *str = *str & ~0x20;
+        ++str;
     }
     return;
 }
@@ -51,7 +60,8 @@ void MyString::setPrintAsUppercase(bool uppercase) {
 
 int MyString::stringLength(char* str) {
     int i = 0;
-    for(; str[i] != '\0'; ++i) {
+    for(; *str != '\0'; ++i) {
+        ++str;
     }
     return i;
 }
@@ -59,15 +69,21 @@ int MyString::stringLength(char* str) {
 MyString MyString::operator+(const MyString& m) {
     int totalLength = m.length + this->length;
     char* str = new char[totalLength];
-    int i = 0;
+    char* strIter = str;
+    char* dataIter = data;
     for (int j = 0; j < this->length; ++j) {
-        str[i] = this->data[j];
-        ++i;
+        *strIter = *dataIter;
+        ++strIter;
+        ++dataIter;
     }
+    
+    dataIter = m.data;
     for (int k = 0; k < m.length; ++k) {
-        str[i] = m.data[k];
-        ++i;
+        *strIter = *dataIter;
+        ++strIter;
+        ++dataIter;
     }
+    
     MyString combined(str);
     return combined;
 }
@@ -78,8 +94,12 @@ MyString MyString::operator--() {
     }
     length = length - 1;
     char* str = new char[length];
+    char* dataIter = data;
+    char* strIter = str;
     for (int i = 0; i < length; ++i) {
-        str[i] = data[i];
+        *strIter = *dataIter;
+        ++strIter;
+        ++dataIter;
     }
     delete [] data;
     data = str;
@@ -93,8 +113,12 @@ MyString MyString::operator--(int i) {
     MyString temp = *this;
     length = length - 1;
     char* str = new char[length];
+    char* strIter = str;
+    char* dataIter = data;
     for (int i = 0; i < length; ++i) {
-        str[i] = data[i];
+        *strIter = *dataIter;
+        ++strIter;
+        ++dataIter;
     }
     delete [] data;
     data = str;
@@ -104,31 +128,40 @@ MyString MyString::operator--(int i) {
 MyString& MyString::operator=(const MyString& m) {
     if (data) {
         delete [] data;
-        data = nullptr;
+        data = NULL;
     }
     length = m.length;
     printAsUppercase = m.printAsUppercase;
     data = new char[length];
+    char* dataIter1 = data;
+    char* dataIter2 = m.data;
     for (int i = 0; i < length; ++i) {
-        data[i] = m.data[i];
+        *dataIter1 = *dataIter2;
+        ++dataIter1;
+        ++dataIter2;
+        //data[i] = m.data[i];
     }
     return *this;
 }
 
 ostream& operator<<(ostream& os, const MyString& str) {
+    char* dataIter = str.data;
     if (MyString::printAsUppercase) {
         char upper;
         for (int i = 0; i < str.length; ++i) {
             //upper[i] = str.data[i] & ~0x20;
-            upper = str.data[i] & ~0x20;
+            upper = *dataIter & ~0x20;
+            ++dataIter;
             os << upper;
         }
         //os << upper << "(" << str.length << ")";
         os << "(" << str.length << ")";
         return os;
     }
+
     for (int i = 0; i < str.length; ++i) {
-        os << str.data[i];
+        os << *dataIter;
+        ++dataIter;
     }
     //os << str.data << "(" << str.length << ")";
     os << "(" << str.length << ")";
@@ -138,9 +171,10 @@ ostream& operator<<(ostream& os, const MyString& str) {
 istream& operator>>(istream& is, MyString& str) {
     const unsigned int maxLength = 255;
     char* buffer = new char[maxLength];
+    char* bufIter = buffer;
     if (str.data) {
         delete [] str.data;
-        str.data = nullptr;
+        str.data = NULL;
         str.length = 0;
     }
     char m;
@@ -152,12 +186,18 @@ istream& operator>>(istream& is, MyString& str) {
         if (m == '\n') {
             break;
         }
-        buffer[i] = m;
+        *bufIter = m;
+        ++bufIter;
         ++i;
     }
     str.data = new char[i];
+    char* dataIter = str.data;
+    bufIter = buffer;
     for (int k = 0; k < i; ++k) {
-        str.data[k] = buffer[k];
+        *dataIter = *bufIter;
+        ++dataIter;
+        ++bufIter;
+        //str.data[k] = buffer[k];
     }
     str.length = i;
 
@@ -168,8 +208,8 @@ bool operator==(const MyString& str1, const MyString& str2) {
     //TODO reimplement this using MyString::stringComp
     // is there anything to compare?
     if (!str1.data || !str1.data) {
-        // if they're both nullptr, they're equal
-        if (str1.data == nullptr && str2.data == nullptr) {
+        // if they're both NULL, they're equal
+        if (str1.data == NULL && str2.data == NULL) {
             return true;
         }
         return false;
@@ -180,10 +220,14 @@ bool operator==(const MyString& str1, const MyString& str2) {
         return false;
     }
 
+    char* dataIter1 = str1.data;
+    char* dataIter2 = str2.data;
     for (int i = 0; i < str1.length; ++i) {
-        if (str1.data[i] != str2.data[i]) {
+        if (*dataIter1 != *dataIter2) {
             return false;
         }
+        ++dataIter1;
+        ++dataIter2;
     }
     return true;
 }
@@ -238,15 +282,28 @@ int main(int argc, char* argv[]) {
         return 0;
     }
     MyString* strings = new MyString[argc - 1];
+    MyString* stringsIter = strings;
+    MyString* lastString;
     for (int i = 0; i < argc-1; ++i) {
-        strings[i] = MyString(argv[i+1]);
+        *stringsIter = MyString(argv[i+1]);
+        ++stringsIter;
+        if (i == (argc-3)) {
+            lastString = stringsIter;
+        }
     }
+    cout << "lastString: " << *lastString << endl;
+    stringsIter = strings;
     for (int i = 0; i < argc-1; ++i) {
-        cout << strings[i] << " ";
+        cout << *stringsIter << " ";
+        //cout << strings[i] << " ";
+        ++stringsIter;
     }
     cout << endl;
-    MyString first(strings[0]);
-    MyString last(strings[argc - 2]);
+    stringsIter = strings;
+    //MyString first(strings[0]);
+    //MyString last(strings[argc - 2]);
+    MyString first(*stringsIter);
+    MyString last(*lastString);
     if (first < last) {
         cout << "The first string (" << first << ") ";
         //cout << "The first string (" << first << ") ";
@@ -263,17 +320,23 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }
     for (int i = 0; i < argc-1; ++i) {
-        cout << --strings[i] << " ";
+        cout << --(*stringsIter) << " ";
+        ++stringsIter;
+        //cout << --strings[i] << " ";
     }
     cout << endl;
     MyString combined;
-    combined = strings[0]-- + --strings[argc - 2];
+    stringsIter = strings;
+    combined = (*stringsIter)-- + --(*lastString);
     cout << combined << endl;
     combined.uppercase();
     cout << combined << endl;
     MyString::setPrintAsUppercase(true);
+    stringsIter = strings;
     for (int i = 0; i < argc-1; ++i) {
-        cout << strings[i] << " ";
+        cout << *stringsIter << " ";
+        ++stringsIter;
+        //cout << strings[i] << " ";
     }
     cout << endl;
 
